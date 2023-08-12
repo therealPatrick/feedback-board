8573c61135e6f5f5207f5b369c08f51d4586df4c
therealPatrick <malamashangala13@gmail.com>
2023-08-12 20:19:06 +0200

    feedback pop feature added

--- app/components/Popup.jsx
+++ app/components/Popup.jsx
@@ -8,16 +8,17 @@ export default function Popup({ setShow, children, title }) {
             </button>
             <div className="w-full">
                 <div className="bg-white md:max-w-2xl md:mx-auto md:rounded-lg overflow-hidden" onClick={e => e.stopPropagation()}>
-                    <div className="relative">
+                    <div className="relative min-h-[40px] md:min-h-0">
                         <button onClick={() => setShow(false)} className="absolute top-4 left-8 md:hidden text-gray-600">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                             </svg>
                         </button>
-
-                        <h2 className="py-4 text-center border-b">
-                            {title}
-                        </h2>
+                        {!!title && (
+                            <h2 className="py-4 text-center border-b">
+                                {title}
+                            </h2>
+                        )}
                     </div>
                     {children}
                 </div>
