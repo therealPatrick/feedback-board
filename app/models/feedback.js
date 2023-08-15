b5522dce9bf5e1020a068e4a10517cefd2e14887
therealPatrick <malamashangala13@gmail.com>
2023-08-15 19:51:59 +0200

    save upload feedbacks

--- app/models/feedback.js
+++ app/models/feedback.js
@@ -3,6 +3,7 @@ const { Schema, models, model } = require("mongoose");
 const feedbackSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
+    uploads: { type: [String] }
 }, { timestamps: true });
 
 
