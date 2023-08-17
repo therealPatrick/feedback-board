1f07013cce0d410615c5887db2d4397cc70354ae
therealPatrick <malamashangala13@gmail.com>
2023-08-17 11:18:45 +0200

    confirm your vote

--- app/components/FeedbackItem.jsx
+++ app/components/FeedbackItem.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+import Popup from "./Popup";
+
 export default function FeedbackItem({ onOpen, title, description, votesCount }) {
+    const [showLoginPopup, setShowLoginPopup] = useState(false);
+    function handleVoteButtonClick(ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+        setShowLoginPopup(true);
+    }
+    const isLoggedIn = false;
     return (
         <a href="" onClick={e => { e.preventDefault(); onOpen(); }} className="my-8 flex gap-8 items-center">
             <div className="flex-grow">
@@ -8,7 +18,14 @@ export default function FeedbackItem({ onOpen, title, description, votesCount })
                 </p>
             </div>
             <div>
-                <button className="shadow-sm border shadow-gray-200 rounded-md py-1 px-4 flex items-center gap-1 text-gray-600">
+                {showLoginPopup && (
+                    <Popup title={'Confirm your vote'} narrow setShow={setShowLoginPopup}>
+                        <div className="p-4">
+                            login Button
+                        </div>
+                    </Popup>
+                )}
+                <button onClick={handleVoteButtonClick} className="shadow-sm border shadow-gray-200 rounded-md py-1 px-4 flex items-center gap-1 text-gray-600">
                     <span className="triangle-vote-up"></span>
                     {votesCount || '0'}
                 </button>
